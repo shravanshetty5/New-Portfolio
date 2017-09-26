236dfff9f0915fd552ee286e0ce2ed49966e9add
Shravan Shetty <shravash@cisco.com>
2017-09-26 10:54:34 -0500

    Move the text content for typwriter to scope.

--- my-web-app/home.component.js
+++ my-web-app/home.component.js
@@ -5,7 +5,7 @@
         controller: function () {
             var vm = this;
 
-            vm.welcomeText = ['Hi there and Welcome to my page.','I am Shravan Shetty and I am a FullStack Developer!!'];
+            vm.welcomeText = ['Hi there and Welcome to my page.','This is Shravan Shetty and I am a FullStack Developer!!'];
         },
         templateUrl: 'my-web-app/home.component.html'
     });
